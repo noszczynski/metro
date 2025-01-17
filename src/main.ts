9e03c4d05a8d0dd0e6520c74695f3615d16d4016
Adam <adam@plenti.app>
2025-01-17 17:49:22 +0100

    comments

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Board } from './board';
 import { Game } from './game';
 
 window.addEventListener('load', () => {
+    // Get HTML elements needed
     const canvas = document.querySelector<HTMLCanvasElement>('#app')!;
     const ctx = canvas.getContext('2d')!;
 
@@ -10,12 +11,15 @@ window.addEventListener('load', () => {
     const BOARD_HEIGHT = 800; // Fixed board height
     const SCALE = 1;
 
+    // Set canvas dimensions and style
     canvas.width = BOARD_WIDTH * SCALE;
     canvas.height = BOARD_HEIGHT * SCALE;
     canvas.style.backgroundColor = '#424242';
 
+    // Create board and game
     const board = new Board(canvas, ctx);
     const game = new Game({ board });
 
+    // Start game loop
     game.start();
 });
