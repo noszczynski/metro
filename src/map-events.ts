56bde82ffddc170870b7dbaa04b5e6a9f113fa43
Adam <adam@plenti.app>
2025-01-20 01:06:08 +0100

    remove Transfers

--- src/map-events.ts
+++ src/map-events.ts
@@ -1,49 +1,10 @@
 export function create_station_marker(id, latlng_orig) {
     var station = L.circleMarker(latlng_orig, {color: "black", opacity: 1.0, fillColor: "white", fillOpacity: 1.0, zIndexOffset: 100}).setRadius(MARKER_RADIUS_DEFAULT);
 
-    station.on('click', function(s_e) {
+    station.on('click', function() {
 
         // Disable new station creation.
         map.off('click', handle_map_click);
-
-        if (N_transfer_state == 1) {
-
-            N_transfer_end = id;
-            N_transfer_state = 0;
-
-            var different_stations = N_transfer_origin != N_transfer_end;
-            var stations_exist = (N_stations[N_transfer_origin].active && N_stations[N_transfer_end]);
-            var origin_latlng = N_stations[N_transfer_origin].marker.getLatLng();
-            var end_latlng = N_stations[N_transfer_end].marker.getLatLng();
-            var point1 = {
-            "type": "Feature",
-                "properties": {},
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [origin_latlng.lat, origin_latlng.lng]
-                }
-            };
-            var point2 = {
-                "type": "Feature",
-                "properties": {},
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [end_latlng.lat, end_latlng.lng]
-                }
-            };
-            var points = {
-                "type": "FeatureCollection",
-                "features": [point1, point2]
-            };
-            var distance = turf.distance(point1, point2, "miles");
-            if (different_stations && stations_exist && distance < 0.5) {
-                var transfer = new Transfer(N_transfer_origin, N_transfer_end);
-                transfer.draw();
-                N_transfers.push(transfer);
-            }
-            generate_route_diagram(N_active_line);
-
-        }
 
         // Wait a second before you can create a new station.
         setTimeout(function() {
@@ -230,12 +191,6 @@ export function transfer_station_event(e) {
 
     var station_id = $(this).attr('id').replace('transfer-', '');
     var station = N_stations[station_id];
-
-    if (N_transfer_state == 0) {
-        N_transfer_origin = station_id;
-        N_transfer_state = 1;
-    }
-
 }
 
 export function line_select_click_handler(td) {
